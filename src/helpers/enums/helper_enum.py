5f88617841146d4ebefa59f2be74021b211eed6e
Valentyn <iwilly17@gmail.com>
2023-05-15 22:34:43 +0300

    small fixes

--- src/helpers/enums/helper_enum.py
+++ src/helpers/enums/helper_enum.py
@@ -2,6 +2,6 @@ import enum
 
 
 class HelperEnum(str, enum.Enum):
-    MAIN_ADMIN_USERNAME: str = "Valentavr"
+    MAIN_ADMIN_USERNAME: str = "demkov"
     MORE_HISTORY: str = 'MORE'
     LIMIT: int = 10
